d59b25cbef2c832ec7870063f05e468e6c651cdb
sergioIgnatev <sergioignatev17@gmail.com>
2022-12-20 10:32:21 +0300

    hover

---
+++ scripts.js
@@ -0,0 +1,4 @@
+let left=document.getElementsByClassName('left');
+let leftimg=document.getElementsByClassName('leftimg');
+left[0].addEventListener('mouseover',function(){leftimg[0].src='./blue/web-page-home.png'})
+left[0].addEventListener('mouseout',function(){leftimg[0].src='./grey/web-page-home.png'})
