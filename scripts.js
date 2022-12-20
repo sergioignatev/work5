da34a2591832d6189117eb40d434b8d0e0aec49e
sergioIgnatev <sergioignatev17@gmail.com>
2022-12-20 20:24:21 +0300

    j

--- scripts.js
+++ scripts.js
@@ -5,7 +5,26 @@ function cl(){
     document.getElementsByClassName('popup')[0].style.display='none'
 }
 
+document.getElementById('switch').addEventListener('click',swit);
 
+
+function swit(){
+    let mount=document.getElementsByClassName('logos')[0];
+    if(mount.style.display=="block"){
+        mount.style.display="none";
+        document.querySelector('i').classList.add("fa-angle-up")
+        document.querySelector('i').classList.remove("fa-angle-down");
+        
+    }else{
+        
+        mount.style.display="block";
+        document.querySelector('i').classList.remove("fa-angle-up")
+        document.querySelector('i').classList.add("fa-angle-down");
+        
+
+
+    }
+}
 let left=document.getElementsByClassName('left');
 let leftimg=document.getElementsByClassName('leftimg');
 left[0].addEventListener('mouseover',function(){leftimg[0].src='./blue/web-page-home.png'})
